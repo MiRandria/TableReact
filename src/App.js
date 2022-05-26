98e299fef9b9b9d97cdfbb3964b09ddfcec62ef1
MiRandria <hei.minosoa@gmail.com>
2022-05-26 20:28:08 +0300

    react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Content from './component/table';
+import Nav_bar from './component/navbar';
+import Side_navbar from './component/sideNav';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body className="sb-nav-fixed">
+      <Nav_bar/>
+      <div id="layoutSidenav">
+        <Side_navbar/>
+        <Content/>
+      </div>
+    </body>
   );
 }
 
